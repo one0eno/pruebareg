b04cddaf0a4802617c1552e67c6983905c645d0d
jorge <jorge.arranz@gmail.com>
2021-10-07 18:16:21 +0200

    Clase Pedido

---
+++ pruebarega/Models/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pruebarega.Models
+{
+    public class Pedido
+    {
+        public List<Item> Productos { get; set; }
+        public string NombreUsuario { get; set; }
+
+        public Pedido()
+        {
+            this.Productos = new List<Item>();
+        }
+    }
+}
